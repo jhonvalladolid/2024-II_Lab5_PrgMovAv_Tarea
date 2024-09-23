824d5a32feec0295f9f3e153edccf1ffb8543a2e
jhonvalladolid <jhonvalladolidoficial@gmail.com>
2024-09-23 17:49:39 -0500

    solucionado al 100%

--- Tarea_PaicesTuristicos/ViewController3.swift
+++ Tarea_PaicesTuristicos/ViewController3.swift
@@ -14,8 +14,12 @@ class ViewController3: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Mostrar la imagen del recurso
         mostrarRecurso()
+        
+        // Agregar funcionalidad de zoom
+        configurarZoomEnImagen()
     }
 
     // Función para mostrar el recurso en imageView
@@ -25,25 +29,36 @@ class ViewController3: UIViewController {
             return
         }
 
-        // Lista de formatos de imagen soportados, incluyendo .avif
-        let formatosSoportados = ["jpg", "png", "jpeg", "bmp", "gif", "avif"]
-
-        var imagenCargada: UIImage?
-
-        // Iterar sobre los formatos para encontrar la imagen
-        for formato in formatosSoportados {
-            if let direccionRecurso = Bundle.main.path(forResource: destino, ofType: formato, inDirectory: "RECURSOSPAICES") {
-                imagenCargada = UIImage(contentsOfFile: direccionRecurso)
-                break  // Detener el ciclo si se encontró un formato válido
+        if let recursosPath = Bundle.main.resourcePath?.appending("/RECURSOSPAICES"),
+           let archivos = try? FileManager.default.contentsOfDirectory(atPath: recursosPath) {
+            
+            // Encontrar el archivo que comienza con el nombre del destino
+            if let archivo = archivos.first(where: { $0.hasPrefix(destino) }),
+               let imagen = UIImage(contentsOfFile: recursosPath + "/" + archivo) {
+                imageView.image = imagen
+            } else {
+                print("No se encontró un archivo para \(destino).")
             }
         }
+    }
 
-        // Si se ha cargado una imagen, mostrarla en imageView
-        if let imagen = imagenCargada {
-            imageView.image = imagen
-        } else {
-            print("El recurso \(destino) no se encontró en ninguno de los formatos soportados.")
-        }
+    // Función para configurar el zoom en la imageView
+    func configurarZoomEnImagen() {
+        // Agregar el gesto de zoom (pinch gesture)
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(didPinch(_:)))
+        imageView.isUserInteractionEnabled = true  // Habilitar la interacción del usuario en imageView
+        imageView.addGestureRecognizer(pinchGesture)
+    }
+
+    // Función para manejar el gesto de zoom
+    @objc func didPinch(_ sender: UIPinchGestureRecognizer) {
+        guard let viewToZoom = sender.view else { return }
+
+        // Ajustar la escala de la vista con el gesto de pellizco
+        viewToZoom.transform = viewToZoom.transform.scaledBy(x: sender.scale, y: sender.scale)
+
+        // Reiniciar el escalado para que sea incremental
+        sender.scale = 1.0
     }
 
 }
